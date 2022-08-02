4045183917d91f50c1f6f394b6b541d37e59aa77
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-08-02 22:50:08 +0300

    make uncontrolled

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOFF";
+import SelfControlledAccordion from "./components/SelfControlledAccordion/Accordion";
+import {SelfControlledRating} from "./components/SelfControlledRating/Rating";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//function declaration
+function App(props: any) {
+    // полезное что-то
+    // обязана вернуть JSX
+    console.log('App rendering')
+    return (
+        <div>
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+
+          {/*  <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={false}/>*/}
+            <SelfControlledAccordion titleValue={"Users"} />
+            <SelfControlledAccordion titleValue={"Users"} />
+            Article 2
+            <SelfControlledRating />
+            <SelfControlledRating />
+           {/* <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>*/}
+
+            <OnOff />
+            <OnOff />
+            <OnOff />
+
+        </div>
+
+
+    );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+
+    console.log('AppTitle rendering')
+    return <div>{props.title}</div>
+
+}
+
 
 export default App;
